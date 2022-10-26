e65b3083e5e5a30a89b380d4e705a3f942c18a15
Katya <ks78tr5@gmail.com>
2022-10-26 17:04:37 +0300

    Create  the class Control, who controls specifies the correctness of the execution of the result inference fuzzy.

--- FuzzyInferenceAlgorithm/Form1.cs
+++ FuzzyInferenceAlgorithm/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace FuzzyInferenceAlgorithm
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+        Control _control; 
+
+        public Form1(Control control)
         {
+            _control = control;
+
             InitializeComponent();
         }
+
+
+
+
     }
 }
